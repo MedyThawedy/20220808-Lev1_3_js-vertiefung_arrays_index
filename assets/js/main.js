6d68565589c76ce3199d0b56403c941fc3175335
MD <dhmedy@googlemail.com>
2022-08-08 17:48:58 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+//Erstelle ein Array numberArray, das Zahlen von 5 bis 10 enthält.
+let numberArray = [5, 6, 7, 8, 9, 10];
+//Gib es in der Konsole aus.
+//Die blau hinterlegten Ziffern geben den Index an.
+console.log(numberArray);
